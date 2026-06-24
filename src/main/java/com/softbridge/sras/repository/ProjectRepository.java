a85ec8239abff8603b316d77d21c2d347e1c7493
K.A.U.M.Kuruppu <upumk2003@gmail.com>
2026-06-25 01:58:28 +0530

    Update ProjectRepository.java

--- src/main/java/com/softbridge/sras/repository/ProjectRepository.java
+++ src/main/java/com/softbridge/sras/repository/ProjectRepository.java
@@ -1,7 +1,12 @@
 package com.softbridge.sras.repository;
 
+import com.softbridge.sras.model.Employee;
 import com.softbridge.sras.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    List<Project> findByProjectManager(Employee projectManager);
 }
