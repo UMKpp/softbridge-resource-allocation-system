e2f9fa1743f40c5547ccb93d221fe8afbe8282d7
K.A.U.M.Kuruppu <upumk2003@gmail.com>
2026-06-24 13:30:05 +0530

    Create AuthenticationFailedException.java

---
+++ src/main/java/com/softbridge/sras/exception/AuthenticationFailedException.java
@@ -0,0 +1,8 @@
+package com.softbridge.sras.exception;
+
+public class AuthenticationFailedException extends RuntimeException {
+
+    public AuthenticationFailedException(String message) {
+        super(message);
+    }
+}
