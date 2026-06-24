91a708b917e0630a5bbdf71a4e57bac1753b36b7
K.A.U.M.Kuruppu <upumk2003@gmail.com>
2026-06-25 01:58:13 +0530

    Update EmployeeSkillRepository.java

--- src/main/java/com/softbridge/sras/repository/EmployeeSkillRepository.java
+++ src/main/java/com/softbridge/sras/repository/EmployeeSkillRepository.java
@@ -4,11 +4,13 @@ import com.softbridge.sras.model.EmployeeSkill;
 import com.softbridge.sras.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeSkillRepository extends JpaRepository<EmployeeSkill, Long> {
 
     List<EmployeeSkill> findByEmployee(Employee employee);
 
-    // Spring Data JPA automatically generates the SQL by analyzing this name!
+    Optional<EmployeeSkill> findByEmployeeAndSkillSkillNameIgnoreCase(Employee employee, String skillName);
+
     List<EmployeeSkill> findBySkillSkillNameAndSkillLevelGreaterThanEqual(String skillName, Integer skillLevel);
 }
