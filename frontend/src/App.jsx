2315075881e53263c9abe9ea0f16772f590c8f67
K.A.U.M.Kuruppu <upumk2003@gmail.com>
2026-06-24 19:31:36 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Employees from "./pages/Employees";
+import AddEmployee from "./pages/AddEmployee.jsx";
 
 function App() {
-  return <Login />;
+  return (
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/add-employee" element={<AddEmployee />} />
+          </Routes>
+      </BrowserRouter>
+
+  );
 }
 
 export default App;
