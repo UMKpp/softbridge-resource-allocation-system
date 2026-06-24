0124692e66ae30ce266af3db71b0e28aa8c4e14f
K.A.U.M.Kuruppu <upumk2003@gmail.com>
2026-06-25 01:57:35 +0530

    Update EmployeeSkillService.java

--- src/main/java/com/softbridge/sras/service/EmployeeSkillService.java
+++ src/main/java/com/softbridge/sras/service/EmployeeSkillService.java
@@ -1,6 +1,7 @@
 package com.softbridge.sras.service;
 
 import com.softbridge.sras.dto.EmployeeSearchResponse;
+import com.softbridge.sras.dto.EmployeeSkillRequest;
 import com.softbridge.sras.dto.EmployeeSkillResponse;
 import com.softbridge.sras.model.EmployeeSkill;
 
@@ -10,11 +11,17 @@ public interface EmployeeSkillService {
 
     EmployeeSkillResponse assignSkill(EmployeeSkill employeeSkill);
 
+    EmployeeSkillResponse addSkillToEmployee(String employeeId, EmployeeSkillRequest request);
+
     List<EmployeeSkillResponse> getSkillsByEmployeeId(String employeeId);
 
     List<EmployeeSearchResponse> searchEmployees(String skill, Integer level);
 
     EmployeeSkillResponse updateSkillLevel(Long id, Integer level);
 
+    EmployeeSkillResponse updateEmployeeSkill(String employeeId, Long id, EmployeeSkillRequest request);
+
     void deleteEmployeeSkill(Long id);
+
+    void deleteEmployeeSkill(String employeeId, Long id);
 }
