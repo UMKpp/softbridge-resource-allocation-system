b27897011e030afcc8a5dcaad142ffcd9fb6eab8
K.A.U.M.Kuruppu <upumk2003@gmail.com>
2026-06-24 21:01:36 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 export default function Dashboard() {
     const navigate = useNavigate();
