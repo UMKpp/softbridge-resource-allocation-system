e3afd3de03fd1b167ab0c5631f9b0576807c4d32
K.A.U.M.Kuruppu <upumk2003@gmail.com>
2026-06-25 01:59:37 +0530

    Create EmployeeProjectAssignmentResponse.java

---
+++ src/main/java/com/softbridge/sras/dto/EmployeeProjectAssignmentResponse.java
@@ -0,0 +1,18 @@
+package com.softbridge.sras.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class EmployeeProjectAssignmentResponse {
+
+    private Long id;
+    private Long projectId;
+    private String projectName;
+    private String employeeId;
+    private String employeeName;
+    private String role;
+    private String assignedBy;
+    private String status;
+}
