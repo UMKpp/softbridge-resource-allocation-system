37b4d9f451ed7039d643b8f5d1c3bfa366155c76
K.A.U.M.Kuruppu <upumk2003@gmail.com>
2026-06-25 01:57:55 +0530

    Create EmployeeProjectAssignmentService.java

---
+++ src/main/java/com/softbridge/sras/service/EmployeeProjectAssignmentService.java
@@ -0,0 +1,20 @@
+package com.softbridge.sras.service;
+
+import com.softbridge.sras.dto.EmployeeProjectAssignmentResponse;
+import com.softbridge.sras.dto.ProjectTeamChangeRequest;
+import com.softbridge.sras.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeProjectAssignmentService {
+
+    EmployeeProjectAssignmentResponse assignEmployee(Long projectId, String employeeId, String role, Employee actor, boolean allowOverride);
+
+    List<EmployeeProjectAssignmentResponse> getProjectTeam(Long projectId, Employee actor, boolean hrAccess);
+
+    List<EmployeeProjectAssignmentResponse> getEmployeeProjects(String employeeId);
+
+    void completeProject(Long projectId, Employee actor);
+
+    EmployeeProjectAssignmentResponse changeTeam(Long projectId, ProjectTeamChangeRequest request, Employee actor);
+}
