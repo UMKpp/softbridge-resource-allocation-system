19c514e149fede460513fca2897fc5095efdbadc
K.A.U.M.Kuruppu <upumk2003@gmail.com>
2026-06-25 18:30:00 +0530

    Update EmployeeSearchResponse.java

--- src/main/java/com/softbridge/sras/dto/EmployeeSearchResponse.java
+++ src/main/java/com/softbridge/sras/dto/EmployeeSearchResponse.java
@@ -3,20 +3,13 @@ package com.softbridge.sras.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 public class EmployeeSearchResponse {
 
     private String employeeId;
     private String fullName;
-    private List<SkillInfo> skills;
-
-    @Data
-    @AllArgsConstructor
-    public static class SkillInfo {
-        private String skillName;
-        private Integer level;
-    }
+    private String username;
+    private String email;
+    private String availabilityStatus;
 }
